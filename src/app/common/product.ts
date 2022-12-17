c68a2e7717ff4d38af118b6c6e89744f18c068d4
Plea_Banshee <llewellynant@gmail.com>
2022-12-17 10:45:51 +0200

    Updated ProductList Component

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,15 +1,6 @@
 export class Product {
 
-  sku: string;
-  name: string;
-  description: string;
-  unitPrice: number;
-  imageUrl: string;
-  active: boolean;
-  unitsInStock: number;
-  dateCreated: Date;
-  lastUpdate: Date;
-
+    // Parameter Properties
     constructor(public sku: string,
                 public name: string,
                 public description: string,
