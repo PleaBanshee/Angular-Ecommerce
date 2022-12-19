f57251e4caf60e71a669a3acb2078c03ade98301
Plea_Banshee <llewellynant@gmail.com>
2022-12-19 18:45:48 +0200

    Added ng-bootstrap to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 // routes for viewing products
 // NB!!! Order of routes are important. The first match is used.
@@ -54,7 +55,8 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes), // for routing
     BrowserModule,
-    HttpClientModule // for HTTP services
+    HttpClientModule, // for HTTP services
+    NgbModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
