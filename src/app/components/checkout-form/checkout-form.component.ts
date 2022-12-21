2667bffd78dafe316d94f1926ad8da338876aa63
Plea_Banshee <llewellynant@gmail.com>
2022-12-21 14:55:20 +0200

    Initialized FormBuilder for Checkout Form

--- src/app/components/checkout-form/checkout-form.component.ts
+++ src/app/components/checkout-form/checkout-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout-form',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout-form.component.css']
 })
 export class CheckoutFormComponent implements OnInit {
+  checkoutFormGroup: FormGroup | undefined;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {  }
 
+  // build the form
   ngOnInit(): void {
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: [''],
+      })
+      // shippingAddress: this.formBuilder.group({
+      //   street: [''],
+      //   city: [''],
+      //   state: [''],
+      //   country: [''],
+      //   zipCode: [''],
+      // }),
+      // billingAddress: this.formBuilder.group({
+      //   street: [''],
+      //   city: [''],
+      //   state: [''],
+      //   country: [''],
+      //   zipCode: [''],
+      // }),
+      // creditCard: this.formBuilder.group({
+      //   cardType: [''],
+      //   nameOnCard: [''],
+      //   cardNumber: [''],
+      //   securityCode: [''],
+      //   expirationMonth: [''],
+      //   expirationYear: [''],
+      // }),
+    });
   }
 
 }
